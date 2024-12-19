04454f0c0f52ee7b36b3700749833cdaa17b0670
David S <161085018+dstraco@users.noreply.github.com>
2024-12-19 10:03:18 -0500

    Array Filters

--- hello.js
+++ hello.js
@@ -1,1 +1,50 @@
-console.log("hello");
+let x = 1;
+let y = 2;
+
+
+let arr = [1,2,3,4];
+let arr2 = ['apple', 'pineapple', 'mango'];
+
+let evenNums = arr.filter((num) =>{
+    if(num % 2 ===0){
+        return num;
+    } 
+})
+
+console.log(evenNums);
+
+
+let companies = [
+    {name: 'Company1', type: 'finance'},
+    {name: 'Company2', type: 'education'}
+];
+
+let finCos = companies.filter((company)=>{
+    if (company.type === 'finance'){
+        return company
+    }
+})
+ 
+console.log(finCos);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
