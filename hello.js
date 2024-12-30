2fc9a7a58b3eba0c48686486a173e8c372496532
David S <161085018+dstraco@users.noreply.github.com>
2024-12-30 15:16:44 -0500

    Replace elements practice.

--- hello.js
+++ hello.js
@@ -1,50 +1,47 @@
-// Shopping List element addition
-
-// inner html: less efficient
-function createListItem(item){
+function replaceFirstItem() {
+    const firstItem = document.querySelector('li:first-child');
+    
     const li = document.createElement('li');
+    li.textContent = 'Replaced First';
     
-    li.innerHTML = `${item}
-          <button class="remove-item btn-link text-red">
-            <i class="fa-solid fa-xmark"></i>
-          </button>`;
-    document.querySelector('.items').appendChild(li);
-}
-
-createListItem('eggs');
-
-//more performant
-function createNewItem(item){
-    const li = document.createElement('li');
-    li.appendChild(document.createTextNode(item));
-
-    const button = createButton('remove-item btn-link text-red');
-    
-    li.appendChild(button);
-    
-    document.querySelector('.items').appendChild(li);
-}
-// called in create new item
-function createButton(classes) {
-    const button = document.createElement('burron');
-    button.className = classes;
-    
-    const icon = createIcon('fa-solid fa-xmark');
-    button.appendChild(icon);
-    
-    return button;
-}
-// called in create new item
-function createIcon(classes) {
-    const icon = document.createElement('i');
-    icon.className = classes;
-    return icon;
+    firstItem.replaceWith(li);
 }
 
 
-createNewItem('Cheese');
-createNewItem('Bacon');
-createNewItem('Protein');
+function replaceSecondItem(){
+    const secondItem = document.querySelector('li:nth-child(2)');
+    
+    secondItem.outerHTML = '<li>Replaced Second</li>';
+}
+
+function replaceAllItems(){
+    const items = document.querySelectorAll('li');
+    
+    items.forEach((item, index) => {
+        
+        if(index===1){
+            item.innerHTML = 'Second Item';
+        } else {
+            item.innerHTML = 'Replace All';
+        }
+    });
+}
+
+
+
+
+
+replaceFirstItem();
+replaceSecondItem();
+replaceAllItems();
+
+
+
+
+
+
+
+
 
 
 
