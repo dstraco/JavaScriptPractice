d94c27f1f5012e5ad71f8431a2a72c2542c7df59
David S <161085018+dstraco@users.noreply.github.com>
2025-01-01 16:35:55 -0500

    Event handling practice complete.

--- hello.js
+++ hello.js
@@ -1,27 +1,71 @@
-const clearBtn = document.querySelector('#clear');
+const logo = document.querySelector('img');
 
 
 
-function onClear() {
-    let itemList = document.querySelector('ul');
-    let items = itemList.querySelectorAll('li');
-    
-    // items.forEach((item) => item.remove());
-    // console.log('Items cleared!')
-    while(itemList.firstChild){
-        itemList.removeChild(itemList.firstChild);
+const onClick = () => {
+    console.log('click event');
+}
+const onDoubleClick = () => {
+
+    if(document.body.style.backgroundColor !== 'teal'){
+        document.body.style.backgroundColor = 'teal';
+        document.body.style.color = 'white';
+    }else{
+        document.body.style.backgroundColor = 'white';
+        document.body.style.color = 'grey';
+    }
+    console.log('double click event');
+}
+
+const onRightClick = () => {
+    alert('I have been right clicked');
+    console.log('RIGHT click event');
+}
+
+const onMouseDown = () => {
+    document.body.style.color = 'goldenrod';
+}
+const onMouseUp = () => {
+    if(document.body.style.backgroundColor === 'teal'){
+        document.body.style.color = 'grey';
+    }else if(document.body.style.backgroundColor !== 'teal'){
+        document.body.style.color = 'grey';
     }
 }
 
+const onMouseWheel = () => {
+    console.log('WHEELIE');
+}
 
-clearBtn.addEventListener('click', onClear);
+const onMouseOver = () => {
+    console.log('Mouse over');
+}
+
+const onDragStart = () => {
+    console.log('Drag start event')
+}
 
 
+const onDrag = () => {
+    console.log('Drag event')
+}
+
+const onDragEnd = () => {
+    console.log('Drag end event')
+}
 
 
-
-
-
+// Event Listeners
+logo.addEventListener('dblclick', onDoubleClick);
+logo.addEventListener('click', onClick);
+logo.addEventListener('contextmenu', onRightClick);
+logo.addEventListener('mousedown', onMouseDown);
+logo.addEventListener('mouseup', onMouseUp);
+logo.addEventListener('wheel', onMouseWheel);
+logo.addEventListener('mouseover', onMouseOver);
+logo.addEventListener('dragstart', onDragStart);
+logo.addEventListener('drag', onDrag);
+logo.addEventListener('dragend', onDragEnd);
 
 
 
