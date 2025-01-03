18b91ade683d983f748c847437506877d9428fb1
David S <161085018+dstraco@users.noreply.github.com>
2025-01-03 15:45:23 -0500

    Input events

--- hello.js
+++ hello.js
@@ -1,40 +1,37 @@
-// Keyboard events
-
 const itemInput = document.getElementById('item-input');
-
-const onKeyPress = () => {
-    console.log('keypress')
-};
-
-const onKeyUp = () => {
-    console.log('keyup')
-};
-
-const onKeyDown = (e) => {
-    console.log(e.key);
-    if(e.key === 'Enter') {
-        alert('You pressed enter');
-    }
-    // KeyCode
-    if(e.keyCode === 13) {
-        alert('You pressed enter');
-    }
-
-    if(e.code === 'Digit1') {
-        console.log('You pressed 1');
-    }
-
-    if(e.repeat) {
-        console.log('You are holding down ' + e.key);
-    }
-};
+const priorityInput = document.getElementById('priority-input');
+const checkbox = document.getElementById('checkbox');
+const heading = document.querySelector('h1');
 
 
+function onInput(e) {
+    heading.textContent = e.target.value;
+}
+
+function onChecked(e) {
+    const isChecked = e.target.checked;
+    heading.textContent = isChecked ? 'Checked' : 'Not Checked';
+}
+
+function onFocus(e) {
+    console.log('Input is focused');
+    itemInput.style.outlineStyle = 'solid';
+    itemInput.style.outlineWidth = '1px';
+    itemInput.style.outlineColor = 'green';
+}
+
+function onBlur(e) {
+    console.log('Input is not focused');
+    itemInput.style.outlineStyle = 'none';
+}
 
 
-// itemInput.addEventListener('keypress', onKeyPress);
-// itemInput.addEventListener('keyup', onKeyUp);
-itemInput.addEventListener('keydown', onKeyDown);
+itemInput.addEventListener('input', onInput);
+priorityInput.addEventListener('input', onInput);
+checkbox.addEventListener('input', onChecked);
+itemInput.addEventListener('focus', onFocus);
+itemInput.addEventListener('blur', onBlur);
+itemInput.addEventListener('hover', onFocus);
 
 
 
