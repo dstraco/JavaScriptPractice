112475cfbd46ee41cc684bf8575ca7478408ac2f
David S <161085018+dstraco@users.noreply.github.com>
2025-01-04 12:10:01 -0500

    Form submission and FormData object. Started on Shopping List project by implementing an addToList function

--- hello.js
+++ hello.js
@@ -1,37 +1,64 @@
-const itemInput = document.getElementById('item-input');
-const priorityInput = document.getElementById('priority-input');
-const checkbox = document.getElementById('checkbox');
-const heading = document.querySelector('h1');
+// Receiving form data from Shopping List
 
+const form = document.getElementById('item-form');
 
-function onInput(e) {
-    heading.textContent = e.target.value;
+// Method 1
+function onSubmit(e){
+    e.preventDefault();
+
+    const item = document.getElementById('item-input').value;
+    const priority = document.getElementById('priority-input').value;
+
+    if (item === '' || priority === '0') {
+        alert('Please fill in all fields');
+        return;
+    }
+
+    console.log(item, priority);
 }
 
-function onChecked(e) {
-    const isChecked = e.target.checked;
-    heading.textContent = isChecked ? 'Checked' : 'Not Checked';
-}
+// Method 2
+function onSubmit2(e) {
+    e.preventDefault();
 
-function onFocus(e) {
-    console.log('Input is focused');
-    itemInput.style.outlineStyle = 'solid';
-    itemInput.style.outlineWidth = '1px';
-    itemInput.style.outlineColor = 'green';
-}
+    const formData = new FormData(form);
 
-function onBlur(e) {
-    console.log('Input is not focused');
-    itemInput.style.outlineStyle = 'none';
+    const item = formData.get('item');
+    const priority = formData.get('priority');
+    console.log(item, priority);
+    // add to list
+    addToList(item);
 }
 
 
-itemInput.addEventListener('input', onInput);
-priorityInput.addEventListener('input', onInput);
-checkbox.addEventListener('input', onChecked);
-itemInput.addEventListener('focus', onFocus);
-itemInput.addEventListener('blur', onBlur);
-itemInput.addEventListener('hover', onFocus);
+// Taking in items from input and adding to the shopping list
+function addToList(item) {
+    newItem = document.createElement('li');
+    ul = document.querySelector('ul');
+
+    // Create text node and create elements button/ icon
+    const text = document.createTextNode(item);
+    const button = document.createElement('button');
+    const icon = document.createElement('i');
+
+    // Style new elements/ nodes
+    button.className = 'remove-item btn-link text-red';
+    button.appendChild(icon);
+    icon.className = 'fa-solid fa-xmark';
+
+    // Append text node and button element to new list element
+    newItem.appendChild(text);
+    newItem.appendChild(button);
+
+    // Append new item to the parent node
+    ul.appendChild(newItem);
+}
+
+
+
+form.addEventListener('submit', onSubmit2);
+
+
 
 
 
